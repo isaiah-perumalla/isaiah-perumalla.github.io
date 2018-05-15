41ae51eff9390c5a90bf018719433e734872817c
isaiahp <isaiahperumalla@gmail.com>
2018-05-15 17:36:30 +0100

    update shell script

--- blog/blog.sh
+++ blog/blog.sh
@@ -4,4 +4,4 @@ $BLOG_HOME/blog_env/bin/pelican-themes -r mc-pelican
 echo "reinstalling theme mc-pelican "
 $BLOG_HOME/blog_env/bin/pelican-themes -i $BLOG_HOME/themes/mc-pelican
 echo "regen content "
-$BLOG_HOME/blog_env/bin/pelican --relative-urls ~/projects/entegrity-blog/blog/content/
+$BLOG_HOME/blog_env/bin/pelican --relative-urls ./content/
