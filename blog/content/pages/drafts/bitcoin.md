9c834a8c17c441942c7a720242487febc26a6b4d
isaiahp <isaiahperumalla@gmail.com>
2020-08-16 19:47:30 +0100

    added bitcoin article

---
+++ blog/content/pages/drafts/bitcoin.md
@@ -0,0 +1,23 @@
+Title: Bitcoin simplified
+Date: 2020-08-16 17:20
+Category: bitcoin cryptocurrency
+
+Bitcoin been around for over 10 years now, and more people are interested in it. This is a very simplified introduction to  understanding what bitcoin is. 
+To understan bitcoin, let first look at traditional cash-based system, 
+1. in general cash is backed by a government and issued by a central bank.
+2. To buy something you would exchange a precise amount of physical cash or the goods or services required
+3. Cash allows us to use numbers to talk about the value of something
+4. unlike credit or debit cards where a bank/third party can track your spending , when you pay in cash* a bank does not come in to the picture, in this regard cash offers better anonymity.
+
+![20 pound note](/imgs/20quid.jpg)
+
+The image above is 20 pound note issued by the bank of england.
+1. it has a serial number
+2. it has additional anti-forgery stuff to esure it not easily replicated.
+
+if we were to digital cash , how would we do this electronically ?
+one way could be create a piece of data representing some a unit of virtual cash, with a digital signature.
+In physical world once you hand once you spent the cash , it gone you cannot respent, however in the digital world, it very simple to make copies of digital cash and keep spending the same note over and over again. 
+To avoid the scenario above each time we spend our digital cash, we could record is some database the serial number of virtual cash, who spent the cash and who received it. This way a receiver of the cash can look up this central database to check if the serial nubmer of virtual cash has been spend already.
+
+
--- blog/content/pages/drafts/code/jvm-jit/plot.py
+++ blog/content/pages/drafts/code/jvm-jit/plot.py
@@ -13,47 +13,55 @@ def parse_csv(fname):
 
 
 #%%
-DIR = r'/home/isaiahp/projects/jvmJIT/'
-df = parse_csv(DIR + 'jit.times.csv')
+def plot(data_file, out_dir):
+    
+    df = parse_csv(data_file)
 
-df_aot = parse_csv(DIR + 'aot.times.csv')
-df_aot_tiered = parse_csv(DIR + 'aot.tiered.csv')
+    # df_aot = parse_csv(DIR + 'aot.times.csv')
+    # df_aot_tiered = parse_csv(DIR + 'aot.tiered.csv')
 
-ax = plt.subplot(facecolor='gray')
+    ax = plt.subplot(facecolor='gray')
 
-plt.yscale('log')
-plt.ylabel('time (nanos)')
-plt.xlabel('run')
-plt.yticks([100, 200, 400, 100_000, 1000_000, 1000_000_0, 100_000_000])
-plt.ylim((min(df.time), max(df_aot.time)))
-plt.plot(df.run_id, df.time, color='yellow', lw=1, label='JIT-Compiled')
-#plt.plot(df_aot.run_id, df_aot.time, color='red', lw=1, label='AOT-Compiled')
+    plt.yscale('log')
+    plt.ylabel('time (nanos)')
+    plt.xlabel('run')
+    plt.yticks([50, 100, 200, 400, 100_000, 1000_000, 1000_000_0, 100_000_000])
+    plt.ylim((min(df.time), max(df.time)))
+    plt.plot(df.run_id, df.time, color='yellow', lw=1, label='JIT-Compiled')
+    #plt.plot(df_aot.run_id, df_aot.time, color='red', lw=1, label='AOT-Compiled')
 
-ax.annotate('Interpreter', xy=(3, 667920), xytext=(5, 1000_000),
-            arrowprops=dict(arrowstyle='->', facecolor='black'))
-
-
-plt.annotate("", xy=(13, 113000), xycoords='data',
-             xytext=(39, 113000), textcoords='data',
-             arrowprops=dict(arrowstyle="<->", connectionstyle="arc3"))
-plt.text(27, 1000, r'C1 JIT',
-         {'color': 'k', 'fontsize': 12, 'ha': 'center', 'va': 'center',
-          'bbox': dict(boxstyle="round", fc="w", ec="k", pad=0.2)})
+    ax.annotate('Interpreter', xy=(3, 667920), xytext=(5, 1000_000),
+                arrowprops=dict(arrowstyle='->', facecolor='black'))
 
 
-plt.annotate("", xy=(40, 1000), xycoords='data',
-             xytext=(99, 1000), textcoords='data',
-             arrowprops=dict(arrowstyle="<->", connectionstyle="arc3"))
-plt.text(65, 1000, r'C2 JIT',
-         {'color': 'k', 'fontsize': 12, 'ha': 'center', 'va': 'center',
-          'bbox': dict(boxstyle="round", fc="w", ec="k", pad=0.2)})
+    plt.annotate("", xy=(13, 113000), xycoords='data',
+                xytext=(39, 113000), textcoords='data',
+                arrowprops=dict(arrowstyle="<->", connectionstyle="arc3"))
+    plt.text(27, 1000, r'C1 JIT',
+            {'color': 'k', 'fontsize': 12, 'ha': 'center', 'va': 'center',
+            'bbox': dict(boxstyle="round", fc="w", ec="k", pad=0.2)})
 
-ax.annotate('Deoptimized', xy=(105, 924303), xytext=(100, 10_000_000),
-            arrowprops=dict(arrowstyle='->', facecolor='black'))
 
-#plt.legend()
-IMG_OUT_DIR = r'/home/isaiahp/projects/isaiah-perumalla.github.io/blog/content/imgs/jvm-jit/'
-plt.savefig(IMG_OUT_DIR +'time.svg')
-plt.show()
+    plt.annotate("", xy=(40, 1000), xycoords='data',
+                xytext=(99, 1000), textcoords='data',
+                arrowprops=dict(arrowstyle="<->", connectionstyle="arc3"))
+    plt.text(85, 1000, r'C2 JIT',
+            {'color': 'k', 'fontsize': 12, 'ha': 'center', 'va': 'center',
+            'bbox': dict(boxstyle="round", fc="w", ec="k", pad=0.2)})
+
+    ax.annotate('Deoptimized', xy=(200, 924303), xytext=(100, 10_000_000),
+                arrowprops=dict(arrowstyle='->', facecolor='black'))
+
+    #plt.legend()
+    
+    plt.savefig(out_dir +'time.svg')
+    plt.show()
+
+
+
+# %%
+DIR = r'/hdd/home/isaiahp/jvmJit/'
+data_file = DIR + 'jit.times.csv'
+plot(data_file, DIR)
 
 
